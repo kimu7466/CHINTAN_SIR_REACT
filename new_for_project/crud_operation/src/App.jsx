ed3cb1022b6a7a98ff1507316feef939428869c1
kimu7466 <kimu7466@gmail.com>
2024-09-28 21:50:33 +0530

    CRUD practice

---
+++ new_for_project/crud_operation/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="mx-auto p-2" style={{width: "200px"}}>
+        Hello world !!!
+      </div>
+    </>
+  );
+}
+
+export default App;
